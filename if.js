a688c8c14c1250cf9cab2fd4ad5d4d1c7cde5a10
Nicolas Marini <nmarini44@gmail.com>
2018-01-24 22:45:33 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,6 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-function takeNumber (number) {if (number > 10) {return number} else {return "Sorry, that's too small"} 
+function takeNumber (number) {if (number > 10)
+{return number} else {return "Sorry, that's too small"} }
 
